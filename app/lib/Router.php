41ad3cec8de4a2839c7c62fce14c84ff88c6980c
Jesse <60900281+jgrobstein@users.noreply.github.com>
2021-07-20 16:13:37 -0400

    Updated router and routes.

--- app/lib/Router.php
+++ app/lib/Router.php
@@ -14,27 +14,58 @@ namespace Iservicesinc\TufPhp;
 
 class Router {
 
-    function maintenance() {
-        if ($_SERVER['maintenance_mode'] == true) {
-            $latte = new \Latte\Engine;
-            $latte->setTempDirectory(BASE_DIR . '/.cache');
-            $latte->render(BASE_DIR . '/views/maintenance.latte', $args = array());
-            exit(0);
+    public static $routes = array();
+    public static $latte = '';
+
+    function __construct() {
+        self::$latte = new \Latte\Engine;
+        self::$latte->setTempDirectory(BASE_DIR . '/.cache');
+    }
+
+    public function addRoute($methods, $pattern, $temp, $kargs = array()) {
+        foreach (explode('|', $methods) as $method) {
+            self::$routes[$method][$pattern] = array(
+                'template' => $temp,
+                'params' => $kargs,
+            );
         }
     }
 
-    function route(string $page, array $args = array()) {
-        $latte = new \Latte\Engine;
-        $latte->setTempDirectory(BASE_DIR . '/.cache');
-        $file = glob(BASE_DIR . "/views/$page*");
-    
-        if(!empty($file)) {
-            $latte->render($file[0], $args);
-            exit(0);
-        } else {
-            return http_response_code(404);
-            exit(1);
+    public function all($pattern, $fn) {
+        self::addRoute('GET|POST|PUT|DELETE|OPTIONS|PATCH|HEAD', $pattern, $fn, $kargs = array());
+    }
+
+    public function init() {
+        if (!isset($_SESSION['salt'])) {
+            $_SESSION['salt'] = hash('sha256', $_SERVER['REMOTE_ADDR'] . time() . $_SERVER['salt'], 0);
+            setcookie('salt', $_SESSION['salt'], time() + 60*60, '/', $_SERVER['SERVER_NAME'], 0, 1);
         }
+        self::maintenance();
+        $url = parse_url($_SERVER['REQUEST_URI']);
+        $path = empty($url['path']) ? '/' : $url['path'];
+        $route = self::$routes[$_SERVER['REQUEST_METHOD']][$path];
+        
+        if ($route !== false) {
+            $template_file = BASE_DIR . "/views/" . $route['template'] . ".latte";
+            self::$latte->render($template_file, $route['params']);
+
+        } else {
+            self::not_found();
+        }
+    }
+
+    function maintenance() {
+        if ($_SERVER['maintenance_mode'] == true) {
+            http_response_code(503);
+            $params['title'] = "Maintenance Mode";
+            self::$latte->render(BASE_DIR . '/views/maintenance.latte', $params);
+            exit(0);
+        }
+    }
+    
+    function not_found() {
+        http_response_code(404);
+        exit(0);
     }
 
 }
--- app/routes.php
+++ app/routes.php
@@ -1,41 +1,10 @@
 <?php
 use \Iservicesinc\TufPhp\Router;
 
-$url = parse_url($_SERVER['REQUEST_URI']);
-$path = $url['path'];
 $router = new Router();
-$router->maintenance();
-if (!isset($_SESSION['salt'])) {
-    $_SESSION['salt'] = hash('sha256', $_SERVER['REMOTE_ADDR'] . time() . $_SERVER['salt'], 0);
-    setcookie('salt', $_SESSION['salt'], time() + 60*60, '/', $_SERVER['SERVER_NAME'], 0, 1);
-}
 
-switch ($path) {
+$router::addRoute('GET', '/', 'home.page', array('title' => 'TUF-php'));
+$router::addRoute('GET', '/docs', 'docs.page', array('title' => 'TUF-php Documentation'));
 
-    case '/':
-    case '/home':
-        $router->route('home.page', array("title" => "Welcome"));
-        break;
-    
-    case '/docs':
-        $router->route('docs.page', array("title" => "Documentation"));
-        break;
-
-    // Maintenance page
-    case '/maintenance':
-        if ($_SERVER['maintenance_mode'] === true) {
-            http_response_code(503);
-            $page = 'maintenance';
-            $params['title'] = "Maintenance Mode";
-            router('maintenance', $params);
-        } else {
-            header("Location: /");
-        }
-        break;
-
-    default:
-        $router->route('404');
-        break;
-}
-        
+$router::init();
 ?>
